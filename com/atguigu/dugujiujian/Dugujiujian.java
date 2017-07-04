a714eb24f70ec579c69378b150ebcbd1ab788ee5
令狐中 <yangzhaohui12@126.com>
2017-07-04 20:46:16 +0800

    dfg

--- com/atguigu/dugujiujian/Dugujiujian.java
+++ com/atguigu/dugujiujian/Dugujiujian.java
@@ -4,6 +4,6 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian");
 		System.out.println("hello dugujiujian67");
-		System.out.println("hello dugujiujian45");
+		System.out.println("hello dugujiujian4");
 	}
 }
