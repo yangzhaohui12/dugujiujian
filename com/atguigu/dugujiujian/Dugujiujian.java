ba608ac5043b565df00f37cf7194d1985629cd6b
令狐中 <yangzhaohui12@126.com>
2017-07-04 20:39:53 +0800

    sdf

--- com/atguigu/dugujiujian/Dugujiujian.java
+++ com/atguigu/dugujiujian/Dugujiujian.java
@@ -3,5 +3,7 @@ package com.atguigu.dugujiujian;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian");
+		System.out.println("hello dugujiujian67");
+		System.out.println("hello dugujiujian45");
 	}
 }
