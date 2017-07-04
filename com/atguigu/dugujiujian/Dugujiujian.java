92fad99ef9451abcb41574146c7d391a8ff9ca05
yzh <huihui@126.com>
2017-07-04 20:21:38 +0800

    ybq pxjianpu

--- com/atguigu/dugujiujian/Dugujiujian.java
+++ com/atguigu/dugujiujian/Dugujiujian.java
@@ -3,5 +3,8 @@ package com.atguigu.dugujiujian;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian");
+		System.out.println("hello dugujiujian2");
+		System.out.println("hello dugujiujian3");
+		System.out.println("hello dugujiujian4");
 	}
 }
